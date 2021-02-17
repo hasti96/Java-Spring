f84c6dbc3d44b952be786a9deb1e7a33beedfebb
Hastimal Paliwal <46267369+hasti96@users.noreply.github.com>
2021-02-17 23:32:35 +0530

    Db working

--- demo/src/main/java/com/example/demo/datasource/PostgresDataSource.java
+++ demo/src/main/java/com/example/demo/datasource/PostgresDataSource.java
@@ -5,14 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
-@ConfigurationProperties("app")
 public class PostgresDataSource {
 
-    @Primary
-    @Bean(name = "datasource")
+    @Bean
+    @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource() {
         return DataSourceBuilder
                 .create()
